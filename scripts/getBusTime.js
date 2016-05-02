4d06959f1750603e2085d39f789de0b7a372d24d
arvinh <arvin0731@gmail.com>
2016-05-03 00:57:23 +0800

    add bus info

--- scripts/getBusTime.js
+++ scripts/getBusTime.js
@@ -1,5 +1,19 @@
+var bus = require("./lib/taipeiBus");
+
 module.exports = (robot) => {
-	robot.hear(/echo (.*)/i, (res) => {
-		robot.messageRoom(res.message.room, res.match[1]);
+	robot.hear(/get (.*) (.*)/i, (res) => {
+		bus(res.match[1], function(err, data) {
+			if (!err) {
+				var result = data['back'].filter(function(item, i) {
+					if (item.name === res.match[2]) {
+						return item;
+					}
+				});
+				robot.messageRoom(res.message.room, JSON.stringify(result));
+				return;
+			} 
+			robot.messageRoom(res.message.room, "sorry, there are some problems..");
+			return;
+		});
 	});
 }
