d1abbe76638e67db3e6e499813fcf972e8e593c5
arvinh <arvin0731@gmail.com>
2016-05-03 08:54:07 +0800

    fine tune wording

--- scripts/getBusTime.js
+++ scripts/getBusTime.js
@@ -9,7 +9,13 @@ module.exports = (robot) => {
 						return item;
 					}
 				});
-				robot.messageRoom(res.message.room, JSON.stringify(result));
+				result.map(function(item, i) {
+					var assembleItem = {
+						direction: (item.direction === 'go') ? '去程' : '回程',
+						status: (item.status.indexOf('分') > -1) ? '還剩下' + item.status : item.status
+					}
+					robot.messageRoom(res.message.room, assembleItem.direction + ' ' + assembleItem.status);
+				});
 				return;
 			} 
 			robot.messageRoom(res.message.room, "sorry, there are some problems..");
